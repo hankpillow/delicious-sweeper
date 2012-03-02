5152f81555eeff24a1baaa2169f63d577dd18094
hankpillow <listas@ialmeida.com>
2012-03-02 19:38:50 -0300

    login and using start param to fecth bookmarks from a specific index

--- api/login.py
+++ api/login.py
@@ -33,14 +33,9 @@ class MainHandler(webapp.RequestHandler):
 			self.response.out.write( json.dumps({"status_code":-4,"message":"null start"}) )
 			return
 
-		validator = re.compile('^[a-zA-Z0-9_.-]+$')
- 		if validator.match(username) is None :
-			self.response.out.write( json.dumps({"status_code":-5,"message":"invalid username characters"} ) )
-			return
-
 		response = {}
 		try:
-			result = urlfetch.fetch(url="https://api.del.icio.us/v1/posts/all",headers={"Authorization": "Basic %s" % base64.b64encode(username+":"+password)},deadline=60, allow_truncated=True)
+			result = urlfetch.fetch(url="https://api.del.icio.us/v1/posts/all?start=start",headers={"Authorization": "Basic %s" % base64.b64encode(username+":"+password)},deadline=60, allow_truncated=True)
 			response = {"status_code":0,"result":result.content}
 		except Exception, e:
 			response = {"status_code":-6, "message":"%s" % e, "url":url}
